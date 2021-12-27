9b7133fa03d5f40ec2b9a0f3216dd6b3f101362e
dhanushdev <dhanushdev201@gmail.com>
2021-12-26 18:58:19 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GUN Chat Dapp Demo
+
+
+
+
+```
+
+npm install
+npm run dev
+```
---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<img src="/logo.jpeg" alt="logo"/>
+<span>bypass chat</span>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+        <h4>join the chat now</h4>
+  {/if}
+</header>
